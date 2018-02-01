2e14b95c417aa842f44fed5fed6ecf8fb0db9364
Abid Hasan <abidhasan@gmail.com>
2018-01-31 22:04:08 -0700

    Initial webcam ddata capturing

--- Day 19 - Webcam Fun/scripts.js
+++ Day 19 - Webcam Fun/scripts.js
@@ -3,3 +3,19 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+//webcam video is piped into video element
+//every 16 ms, a screenshot is piped into canvas
+//canvas is responsible for making funky effects
+//audio is just a fun sound track
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream => {
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play()
+    })
+}
+
+getVideo()
