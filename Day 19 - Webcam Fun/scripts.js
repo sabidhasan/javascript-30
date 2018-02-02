84cb83d6256f1b36e6193df7085582e8698ed296
Abid Hasan <abidhasan@gmail.com>
2018-02-01 19:44:59 -0700

    Day 19 ctd

--- Day 19 - Webcam Fun/scripts.js
+++ Day 19 - Webcam Fun/scripts.js
@@ -13,9 +13,49 @@ const snap = document.querySelector('.snap');
 function getVideo() {
   navigator.mediaDevices.getUserMedia({video: true, audio: false})
     .then(localMediaStream => {
-      video.src = window.URL.createObjectURL(localMediaStream);
-      video.play()
+      //Make an URL of the video data
+      const videoData = window.URL.createObjectURL(localMediaStream)
+      //set video player srouce to this data
+      video.src = videoData;
+      //make video constantly play
+      video.play();
     })
+    .catch(err => {
+      console.error("OH NO! Please allow it!");
+    });
 }
 
-getVideo()
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  //set canvas width and height
+  canvas.width = width;
+  canvas.height = height;
+
+  //update canvas every 32 ms
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 32);
+}
+
+function takePhoto() {
+  //play the audio
+  snap.currentTime = 0;
+  snap.play();
+
+  //take photo out of canvas
+  const photo = canvas.toDataURL("image/jpeg");
+  //create link (for image), and set its properties
+  const link = document.createElement("a");
+  link.href = photo;
+  link.setAttribute('download', 'me!');
+  link.textContent = "Download Image";
+  link.innerHTML = `<img src=${photo} alt="" />`;
+  //insert into DOM
+  strip.insertBefore(link, strip.firstChild)
+}
+
+//get webcam set up, start painting to canvas when ready
+getVideo();
+video.addEventListener('canplay', paintToCanvas)
